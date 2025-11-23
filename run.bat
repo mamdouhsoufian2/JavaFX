2cef220c2102285d72166234de04aa14acf3b477
Mamdouh Soufiane <mamdouhsoufiane22@gmail.com>
2025-11-23 19:59:53 +0100

    Ajout du .gitignore et retrait des fichiers générés

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVAFX=C:\Users\admin\Downloads\openjfx-25.0.1_windows-x64_bin-sdk\javafx-sdk-25.0.1\lib
-mvn clean package
-
-java --module-path "%JAVAFX%" --add-modules javafx.controls,javafx.fxml -cp target/classes com.example.xmlvalidator.MainApp
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-JAVAFX=/opt/javafx-sdk-21/lib
-mvn clean package
-java --module-path "$JAVAFX" --add-modules javafx.controls,javafx.fxml -cp target/classes com.example.xmlvalidator.MainApp
